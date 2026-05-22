20a2e73a218a3b0bd906b6964058441a98cc4a8b
tesii <130172829+tesii@users.noreply.github.com>
2026-05-22 22:25:20 +0200

    Update user.php

--- backend/user.php
+++ backend/user.php
@@ -1,1 +1,15 @@
+<?php
+// database.php
 
+$host = 'localhost';  // Your database server
+$db = 'mynewdb';  // Your database name
+$user = 'root';       // Your database username
+$pass = '';           // Your database password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+?>
